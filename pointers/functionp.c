9ae03521a0e0216041dff325d102a98e2347bdab
Alec Meyer <11meyal@gmail.com>
2021-01-10 15:20:52 -0600

    followed JS style map fn

--- pointers/functionp.c
+++ pointers/functionp.c
@@ -3,17 +3,17 @@
 #include <stdlib.h>
 
 
-char tolowercase (char character) {
+char tolowercase (char character, size_t length, char* string) {
     return character + 32;
 }
 
 // Call free when done
-char* map(char* string, char (*mapFn)(char c)) {
+char* map(char* string, char (*mapFn)(char c, size_t length, char* string)) {
     size_t len = strlen(string);
     char* mappedstring = malloc(len);
 
     for(size_t i = 0; i < len; i++, mappedstring++, string++) {
-        *mappedstring = mapFn(*string);
+        *mappedstring = mapFn(*string, len, string - i);
     }
 
     return mappedstring - len;
